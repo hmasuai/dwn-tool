15f7a5fcf3ad7cce184f2a3b145cb3a2061e0ffa
hmasusi <143053966+hmasuai@users.noreply.github.com>
2024-06-05 19:08:14 -0500

    fixed creating final.txt

--- grabsound.sh
+++ grabsound.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env zsh
-python3 space_replace.py | sed 's/Track+Name+Artist+Name(s)//g;s/^/ytsearch:/' < final.txt | yt-dlp --batch-file - -x -P "$PWD/songs" -o "%(title)s.%(ext)s"
+python3 space_replace.py && sleep 1
+sed 's/Track+Name+Artist+Name(s)//g;s/^/ytsearch:/' < final.txt | yt-dlp --batch-file - -x -P "$PWD/songs" -o "%(title)s.%(ext)s"
