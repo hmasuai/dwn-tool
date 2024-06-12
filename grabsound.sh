0ef14227428796af63a11777ddc9376c49b78993
hmasusi <143053966+hmasuai@users.noreply.github.com>
2024-06-12 11:49:52 -0500

    Update grabsound.sh

--- grabsound.sh
+++ grabsound.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env zsh
+#Create variable for user input of title
+printf "%s" "Enter desired folder title: " 
+read title
 python3 space_replace.py && sleep 1 # Run space_replace.py then pause for 1 second so final.txt can be created
-# Delete spreadsheet header then add ytsearch: to the song title + artist name pipe that result to yt-dlp options are audio only, create a folder, and name it based on title.
-sed 's/Track+Name+Artist+Name(s)//g;s/^/ytsearch:/' < final.txt | yt-dlp --batch-file - -x -P "$PWD/songs" -o "%(title)s.%(ext)s"
-# Pause for 1 second then delete the other created files
+# Delete spreadsheet header then add ytsearch: to the song title + artist name pipe that result to yt-dlp
+sed 's/Track+Name+Artist+Name(s)//g;s/^/ytsearch:/' < final.txt | yt-dlp --batch-file - -x -P "$PWD/${title}" -o "%(title)s.%(ext)s"
+#yt-dlp options are audio only, create a folder, and name it based on title.
 sleep 1 && rm final.txt song.csv song.txt
